188e50ae09329fb2dd7a50024db9f5196eb72bd4
storm-codes <cjxninja@gmail.com>
2022-09-25 22:34:02 +0200

    lesson-1-forreal
    forgot to save some bits

--- components/todo-appy.js
+++ components/todo-appy.js
@@ -5,16 +5,19 @@ import {
 
 class ToDoAppComp extends LitElement {
   render() {
-    return html` 
-        <div>
-          <td-counter count="3" label="Chords" ></td-counter>
-          <td-counter count="6" label="Working Flash Drives" ></td-counter>
-          <td-counter count="99" label="Corrupted Flash Drives" ></td-counter>
-          <td-counter count="43" label="Wrong Answers" ></td-counter>
-          <td-counter count="42" label="Answers Life the Universe and Everything" ></td-counter>
-          <td-counter count="-110" label="Electricity" ></td-counter> 
-            </div> 
-            `;
+    return html`
+      <div>
+        <td-counter count="3" label="Chords"></td-counter>
+        <td-counter count="6" label="Working Flash Drives"></td-counter>
+        <td-counter count="99" label="Corrupted Flash Drives"></td-counter>
+        <td-counter count="43" label="Wrong Answers"></td-counter>
+        <td-counter
+          count="42"
+          label="Answers Life the Universe and Everything"
+        ></td-counter>
+        <td-counter count="-110" label="Electricity"></td-counter>
+      </div>
+    `;
   }
 }
 
--- index.js
+++ index.js
@@ -3,4 +3,4 @@ console.log(`testing testing 123`);
 import "./components/counter-element.js";
 import "./components/lit-counter-element.js";
 import "./components/todo-appy.js";
-import "./components/td-counter.js"
+import "./components/td-counter.js";
